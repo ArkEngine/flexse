fadfce170d9b99fa93d0e0152814b0042d4bb677
scenbuffalo@gmail.com <scenbuffalo@gmail.com@b3441f12-2e28-7e8c-f244-a24f140ef41e>
2011-06-14 14:09:26 +0000

    add creat_sign

---
+++ trunk/utils/creat_sign/creat_sign.h
@@ -0,0 +1,7 @@
+#include <stdint.h>
+
+uint32_t BKDRHash(const char *str, const uint32_t length);
+
+uint32_t APHash(const char *str, const uint32_t length);
+
+void creat_sign_64(const char* str, const uint32_t length, uint32_t* sign1, uint32_t* sign2);
