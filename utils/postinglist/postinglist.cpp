23a0f756201aa8e941c779800ea37a413c2a9af9
scenbuffalo@gmail.com <scenbuffalo@gmail.com@b3441f12-2e28-7e8c-f244-a24f140ef41e>
2011-06-15 15:26:38 +0000

    meke done

--- utils/postinglist/postinglist.cpp
+++ utils/postinglist/postinglist.cpp
@@ -1,23 +1,37 @@
+#include <assert.h>
+#include <string.h>
 #include "postinglist.h"
 
 
-postinglist :: postinglist(/*Json::Value config*/)
+postinglist :: postinglist(const uint32_t posting_cell_size /*json config*/)
 {
 	// 初始化bucket
+    m_postinglist_cell_size = posting_cell_size;
 	m_bucket_size = 0x100000;
-	m_bucket_mask = bucket_size - 1;
-	m_bucket = (uint32_t*)calloc(bucket_size, sizeof(uint32_t));
+	m_bucket_mask = m_bucket_size - 1;
+	m_bucket = (uint32_t*)calloc(m_bucket_size, sizeof(uint32_t));
 	// 初始化headlist
 	const uint32_t headlist_size = 0x1000000;
 	m_headlist = (term_head_t*)malloc(headlist_size*sizeof(term_head_t));
 	m_headlist_use = 0;
+
+    uint32_t msiz = 256;
+    for (uint32_t i=0; i<sizeof(m_memsize)/sizeof(m_memsize[0]); i++)
+    {
+        m_memsize[i] = msiz;
+        msiz = msiz * 2;
+    }
 }
+
 int32_t postinglist :: get (const uint64_t& key, char* buff, const uint32_t length)
 {
+    assert(key && buff && length);
+    return 0;
 }
-int32_t postinglist :: set (const uint64_t& key, char* buff, const uint32_t length)
+
+int32_t postinglist :: set (const uint64_t& key, char* buff)
 {
-	uint32_t bucket_no = key & (uint64_t)m_bucket_mask;
+	uint32_t bucket_no = (uint32_t)(key & m_bucket_mask);
 	uint32_t head_list_offset = m_bucket[bucket_no];
 	bool found = false;
 	while(head_list_offset != END_OF_LIST)
@@ -27,29 +41,49 @@ int32_t postinglist :: set (const uint64_t& key, char* buff, const uint32_t leng
 		{
 			found = true;
 			// 看看当前的memblock是否能够放下
-			if (phead->left_size <= length)
+			if (phead->mem_link->left_size <= m_postinglist_cell_size)
 			{
-				char* dest = &(((char*)list_buffer)[phead->self_size-phead->left_size]);
-				memcpy(dest, buff, length);
-				phead->left_size -= length;
+				char* dest = &(((char*)&phead->mem_link[1])[phead->mem_link->self_size - phead->mem_link->left_size]);
+				memcpy(dest, buff, m_postinglist_cell_size);
+				phead->mem_link->left_size -= m_postinglist_cell_size;
 			}
 			else
 			{
 				term_head_t* newhead = (term_head_t*)m_memblocks->AllocMem(m_memsize[0]);
 				MyToolThrow("memblocks allocmem fail.");
-				newhead->left_size = m_memsize[0] - sizeof(term_head_t);
-				newhead->self_size = m_memsize[0];
-				newhead->next_size = phead->self_size;
+				newhead->mem_link->left_size = m_memsize[0] - sizeof(term_head_t);
+				newhead->mem_link->self_size = m_memsize[0];
+				newhead->mem_link->next_size = phead->mem_link->self_size;
 			}
 		}
 	}
 	if (! found)
 	{
 	}
+    return 0;
 }
-int32_t postinglist :: sort();
-int32_t postinglist :: dump();
-int32_t postinglist :: merge();
-int32_t postinglist :: begin();
-int32_t postinglist :: next(key_t& key, char* buff, const uint32_t length);
-bool    postinglist :: isend();
+int32_t postinglist :: sort()
+{
+    return 0;
+}
+int32_t postinglist :: dump()
+{
+    return 0;
+}
+int32_t postinglist :: merge()
+{
+    return 0;
+}
+int32_t postinglist :: begin()
+{
+    return 0;
+}
+int32_t postinglist :: next(const uint64_t& key, char* buff, const uint32_t length)
+{
+    assert(key && buff && length);
+    return 0;
+}
+bool postinglist :: isend()
+{
+    return 0;
+}
--- utils/postinglist/postinglist.h
+++ utils/postinglist/postinglist.h
@@ -4,34 +4,48 @@
 class postinglist
 {
     private:
+        static const uint32_t END_OF_LIST = 0xFFFFFFFF;
         uint32_t*  m_bucket;
-        uint32_t*  m_list;
+        uint32_t*  m_sortlist;
         memblocks* m_memblocks;
         int32_t    mem_merge();
+        uint32_t   m_postinglist_cell_size;
+        uint32_t   m_bucket_size;
+        uint32_t   m_bucket_mask;
+        uint32_t   m_memsize[8];
 
-		typedef struct _term_head_t
+		struct mem_link_t
 		{
-			uint64_t sign64;
-			void*    list_buffer;
-			uint32_t list_num;
-			uint32_t list_buffer_size;
-			uint32_t next;
-		}term_head_t;
-
-		typedef struct _mem_link_t
-		{
-			uint32_t    left_size ;
-			uint32_t    self_size;
-			uint32_t    next_size;
+			uint32_t    left_size; ///< 剩余内存的大小
+			uint32_t    self_size; ///< 这个内存块本身大小
+			uint32_t    next_size; ///< 下个内存块的本身大小
 			mem_link_t* next;
-		}mem_link_t;
+		};
+
+		struct term_head_t
+		{
+			uint64_t     sign64;
+			uint32_t     list_num;
+			uint32_t     list_buffer_size;
+			uint32_t     next;
+			mem_link_t*  mem_link;
+		};
+
+        term_head_t* m_headlist;
+        uint32_t     m_headlist_use;
+
+        postinglist();
+        postinglist(const postinglist&);
+
     public:
-        int32_t get (const key_t& key, char* buff, const uint32_t length);
-        int32_t set (const key_t& key, char* buff, const uint32_t length);
+        postinglist(const uint32_t posting_cell_size);
+        ~postinglist();
+        int32_t get (const uint64_t& key, char* buff, const uint32_t length);
+        int32_t set (const uint64_t& key, char* buff);
         int32_t sort();
         int32_t dump();
         int32_t merge();
         int32_t begin();
-        int32_t next(key_t& key, char* buff, const uint32_t length);
+        int32_t next(const uint64_t& key, char* buff, const uint32_t length);
         bool    isend();
 };
