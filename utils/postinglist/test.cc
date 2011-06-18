e4e8d73c96b3bab9219ea2ef88db16215e2ba9a9
scenbuffalo@gmail.com <scenbuffalo@gmail.com@b3441f12-2e28-7e8c-f244-a24f140ef41e>
2011-06-18 13:59:51 +0000

    posting list bugfix

--- utils/postinglist/test.cc
+++ utils/postinglist/test.cc
@@ -5,12 +5,13 @@
 
 int main(const int argc, char** argv)
 {
-    if (argc != 2)
+    if (argc != 3)
     {
-        printf("./test SIZE\n");
+        printf("./test KEYSIZE VALUESIZE\n");
         return 1;
     }
-    const uint32_t SIZE = atoi(argv[1]);
+    const uint32_t KEYSIZE = atoi(argv[1]);
+    const uint32_t VLUSIZE = atoi(argv[2]);
     const uint32_t cell_size     = sizeof(uint32_t);
     const uint32_t bucket_size   = 20;
     const uint32_t headlist_size = 0x1000000;
@@ -35,18 +36,39 @@ int main(const int argc, char** argv)
 //        assert(i == t);
 //    }
 
-    const uint64_t ukey = 4;
-    uint32_t* ubuff = (uint32_t*)malloc(SIZE* 2 * sizeof(uint32_t));
-    for (uint32_t i=0; i<SIZE; i++)
+    uint32_t* ubuff = (uint32_t*)malloc(VLUSIZE* 2 * sizeof(uint32_t));
+    for (uint32_t ukey=0; ukey<KEYSIZE; ukey++)
     {
-        mypostinglist.set(ukey, (char*)&i);
+        for (uint32_t i=0; i<VLUSIZE; i++)
+        {
+            mypostinglist.set(ukey, (char*)&i);
+        }
     }
-    int32_t rnum = mypostinglist.get(ukey, (char*)ubuff, SIZE * 2 * sizeof(uint32_t));
-    printf("rnum: %u\n", rnum);
-    for (uint32_t i=0; i<SIZE; i++)
+//    printf("----------\n");
+    for (uint32_t ukey=0; ukey<KEYSIZE; ukey++)
     {
-        assert(ubuff[i] == (SIZE - 1 - i));
+        int32_t rnum = mypostinglist.get(ukey, (char*)ubuff, VLUSIZE * 2 * sizeof(uint32_t));
+//        printf("rnum: %u\n", rnum);
+        for (uint32_t i=0; i<VLUSIZE; i++)
+        {
+            assert(ubuff[i] == (VLUSIZE - 1 - i));
+        }
     }
     free(ubuff);
+
+//    const uint32_t ukey = 0;
+//    uint32_t* ubuff = (uint32_t*)malloc(SIZE* 2 * sizeof(uint32_t));
+//    for (uint32_t i=0; i<SIZE; i++)
+//    {
+//        mypostinglist.set(ukey, (char*)&i);
+//    }
+//    printf("----------\n");
+//    int32_t rnum = mypostinglist.get(ukey, (char*)ubuff, SIZE * 2 * sizeof(uint32_t));
+//    printf("rnum: %u\n", rnum);
+//    for (uint32_t i=0; i<SIZE; i++)
+//    {
+//        assert(ubuff[i] == (SIZE - 1 - i));
+//    }
+//    free(ubuff);
     return 0;
 }
