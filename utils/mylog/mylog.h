3a125b7b36b0e15dff9a136a4acbc20292c457fc
scenbuffalo@gmail.com <scenbuffalo@gmail.com@b3441f12-2e28-7e8c-f244-a24f140ef41e>
2011-06-18 05:38:18 +0000

    add mylog

--- utils/mylog/mylog.h
+++ utils/mylog/mylog.h
@@ -6,13 +6,14 @@
  * Name: mylog - The Mutil-Thread Logger
  * Features:
  * (1) Mutil-Thread
- * (2) ¿¿¿¿¿¿¿¿¿¿, ¿¿ 1G
- * (3) ¿¿4¿¿¿¿¿:
- *     -0- DEBUG, ¿¿¿¿
- *     -1- ROUTN, ¿¿¿¿
- *     -2- ALARM, ¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿
- *     -3- FATAL, ¿¿¿¿¿¿¿¿¿¿¿¿¿
- * (4) ¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿
+ * (2) logfile maxsize limited to 1G
+ * (3) 4 level
+ *     -0- DEBUG
+ *     -1- ROUTN
+ *     -2- ALARM
+ *     -3- FATAL
+ * (4) auto switch into new file when file inc upto 1G
+ * (5) switch by time?
  */
 class mylog
 {
@@ -23,11 +24,11 @@ class mylog
         static const uint32_t LogContentMaxLen = 2048;
         static const char* const strLogName;
         static const char* LevelTag[];
-        uint32_t m_level; // ÈÕÖ¾¼¶±ð
-        uint32_t m_file_size; // ÈÕÖ¾´óÐ¡
-        char m_path[LogNameMaxLen]; // ÈÕÖ¾Â·¾¶
-        int m_log_fd; // µ±Ç°ÈÕÖ¾µÄÎÄ¼þÃèÊö·û
-        pthread_mutex_t m_lock; // Ð´ÎÄ¼þËø
+        uint32_t m_level;
+        uint32_t m_file_size;
+        char m_path[LogNameMaxLen];
+        int m_log_fd;
+        pthread_mutex_t m_lock;
         char m_timebuff[TimeBuffMaxLen];
         const char* GetTimeNow();
         void LogFileSizeCheck();
@@ -35,12 +36,30 @@ class mylog
         void Mkdirs(const char* dir); // µÝ¹éµÄ´´½¨ÎÄ¼þ¼Ð
         void WriteNByte(int fd, const char* buff, uint32_t size);
         mylog();
+        mylog(const mylog&);
+        static mylog* m_mylog;
     public:
         enum { DEBUG = 0, ROUTN, ALARM, FATAL, };
-        mylog(const uint32_t level, const uint32_t size, const char* logname);
+        static mylog* getInstance();
+        void setlog(const uint32_t level, const uint32_t size, const char* logname);
         ~mylog();
         void WriteLog(const uint32_t mylevel, const char* file,
                 const uint32_t line, const char* func, const char *format, ...);
 };
+
+#define SETLOG(level, size, logname) \
+        mylog::getInstance()->setlog(level, size, logname)
+
+#define FATAL(fmt, msg ...) \
+        mylog::getInstance()->WriteLog(mylog::FATAL, __FILE__, __LINE__, __func__, fmt, ##msg)
+
+#define ALARM(fmt, msg ...) \
+        mylog::getInstance()->WriteLog(mylog::ALARM, __FILE__, __LINE__, __func__, fmt, ##msg)
+
+#define ROUTN(fmt, msg ...) \
+        mylog::getInstance()->WriteLog(mylog::ROUTN, __FILE__, __LINE__, __func__, fmt, ##msg)
+
+#define DEBUG(fmt, msg ...) \
+        mylog::getInstance()->WriteLog(mylog::DEBUG, __FILE__, __LINE__, __func__, fmt, ##msg)
 
 #endif
