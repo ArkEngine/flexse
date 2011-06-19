6e23ca53e84eb18c4af93adf81b9729841ce2e78
scenbuffalo@gmail.com <scenbuffalo@gmail.com@b3441f12-2e28-7e8c-f244-a24f140ef41e>
2011-06-19 12:38:52 +0000

    add detaildb test

--- utils/detaildb/test.cc
+++ utils/detaildb/test.cc
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include "detaildb.h"
 
 int main()
 {
+    detaildb dtdb("./data/", "test");
+    const uint32_t SIZE = 100;
+    char* str = (char*)malloc(SIZE);
+    memset(str, 'A', SIZE);
+    for (uint32_t i=0; i<100; i++)
+    {
+        dtdb.set(i, str, SIZE);
+    }
     printf("hello world.");
     return 0;
 }
