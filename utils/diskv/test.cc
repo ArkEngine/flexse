bbef602de81d7cdb4c93014fc4cbc906aa16ef22
scenbuffalo@gmail.com <scenbuffalo@gmail.com@b3441f12-2e28-7e8c-f244-a24f140ef41e>
2011-06-19 10:17:28 +0000

    diskv test add & bugfix

--- utils/diskv/test.cc
+++ utils/diskv/test.cc
@@ -1,8 +1,28 @@
 #include <stdio.h>
-
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+#include "diskv.h"
 
 int main()
 {
     printf("hello world\n");
+    diskv dv("./data/", "test");
+    diskv :: diskv_idx_t idx;
+    const uint32_t SIZE = 16*1024*1024;
+    char* str = (char*)malloc(SIZE);
+    memset (str, 'A', SIZE);
+    for (uint32_t i=0; i<100; i++)
+    {
+        dv.set(idx, str, SIZE);
+    }
+    printf("file_no[%u] offset[%u] data_len[%u]\n",
+            idx.file_no, idx.offset, idx.data_len);
+    dv.get(idx, str, SIZE);
+    for (uint32_t i=0; i<SIZE; i++)
+    {
+        assert('A' == str[i]);
+    }
+    free(str);
     return 0;
 }
