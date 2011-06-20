a237768a10034c1e14422ca2c9621b758ba8466d
scenbuffalo@gmail.com <scenbuffalo@gmail.com@b3441f12-2e28-7e8c-f244-a24f140ef41e>
2011-06-20 16:11:09 +0000

    server run all thread

---
+++ trunk/flexse/merge_thread.h
@@ -0,0 +1,6 @@
+#ifndef _MERGE_THREAD_H_
+#define _MERGE_THREAD_H_
+
+void* merge_thread(void* args);
+
+#endif
---
+++ trunk/flexse/update_thread.h
@@ -0,0 +1,6 @@
+#ifndef _UPDATE_THREAD_H_
+#define _UPDATE_THREAD_H_
+
+void* update_thread(void* args);
+
+#endif
