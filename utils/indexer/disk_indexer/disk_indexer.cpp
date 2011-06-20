0745e4be52da87033912a6ccaf202b0adb691718
scenbuffalo@gmail.com <scenbuffalo@gmail.com@b3441f12-2e28-7e8c-f244-a24f140ef41e>
2011-06-20 03:02:11 +0000

    disk_indexer add freeze status

--- utils/indexer/disk_indexer/disk_indexer.cpp
+++ utils/indexer/disk_indexer/disk_indexer.cpp
@@ -16,11 +16,19 @@ disk_indexer :: disk_indexer(const char* dir, const char* iname)
     char filename[MAX_FILE_LENGTH];
     snprintf(filename, sizeof(filename), FORMAT_SECOND_INDEX, dir, iname);
     int fd = open(filename, O_RDONLY);
-    MyThrowAssert(fd != -1);
-    second_index_t sindex;
-    while(sizeof(sindex) == read(fd, &sindex, sizeof(sindex)))
+    if (fd == -1)
     {
-        second_index.push_back(sindex);
+        ALARM("a new disk_indexer, set freeze as FALSE.");
+        m_freeze = false;
+    }
+    else
+    {
+        second_index_t sindex;
+        while(sizeof(sindex) == read(fd, &sindex, sizeof(sindex)))
+        {
+            second_index.push_back(sindex);
+        }
+        m_freeze = (0 != second_index.size());
     }
     close(fd);
 }
@@ -31,6 +39,9 @@ disk_indexer :: ~disk_indexer()
 
 int32_t disk_indexer :: get_posting_list(const char* strTerm, char* buff, const uint32_t length)
 {
+    // 一旦调用get方法，则变为只读状态
+    m_freeze = (m_freeze) ? true: false;
+
     int len = strlen(strTerm);
     if (0 == len || NULL == buff || length == 0)
     {
@@ -38,19 +49,51 @@ int32_t disk_indexer :: get_posting_list(const char* strTerm, char* buff, const 
         return -1;
     }
 
+    // (1) 先读取二级索引，得到在fileblock中的第几块中
     second_index_t si;
     creat_sign_64(strTerm, len, &si.ikey.uint1, &si.ikey.uint2);
     vector<second_index_t>::iterator bounds;
     bounds = lower_bound (second_index.begin(), second_index.end(), si);
+    if (bounds == second_index.end())
+    {
+        ALARM("strTerm[%s]'s sign[%llu] too big.", strTerm, si.ikey.sign64);
+        return -1;
+    }
+    // (2) 根据milestone读取fileblock中的连续块
+    // (3) 执行二分查找
+    // (4) 得到了diskv中的diskv_idx_t
+    // (5) 读取索引
     return 0;
 }
 
 int32_t disk_indexer :: set_posting_list(const uint32_t id, const ikey_t& ikey,
         const char* buff, const uint32_t length)
 {
+    if (m_freeze)
+    {
+        ALARM("disk_indexer is FREEZON, SO COLD.");
+        return -1;
+    }
     fb_index_t fi;
     MyThrowAssert(0 == m_diskv.set(fi.idx, buff, length));
     fi.ikey = ikey;
     MyThrowAssert(0 == m_fileblock.set(id, &fi));
+    // 记住最后一个milestone
+    m_last_si.milestone = id;
+    m_last_si.ikey      = ikey;
+    // set milestone
+    if (0 == (id % TERM_MILESTONE))
+    {
+        second_index.push_back(m_last_si);
+    }
     return 0;
+}
+
+void disk_indexer :: set_finish()
+{
+    m_freeze = true;
+    if (0 != (m_last_si.milestone % TERM_MILESTONE))
+    {
+        second_index.push_back(m_last_si);
+    }
 }
--- utils/indexer/disk_indexer/disk_indexer.h
+++ utils/indexer/disk_indexer/disk_indexer.h
@@ -10,11 +10,6 @@ using namespace std;
 class disk_indexer : public indexer
 {
     private:
-        static const char* const FORMAT_SECOND_INDEX;
-        static const uint32_t MAX_FILE_LENGTH = 128;
-        fileblock m_fileblock;
-        diskv     m_diskv;
-
         struct fb_index_t
         {
             ikey_t             ikey;
@@ -31,15 +26,34 @@ class disk_indexer : public indexer
             }
         };
 
-        vector<second_index_t> second_index;
+        static const char* const FORMAT_SECOND_INDEX;
+        static const uint32_t MAX_FILE_LENGTH = 128;
+        static const uint32_t TERM_MILESTONE  = 1000;
+        fileblock m_fileblock;
+        diskv     m_diskv;
+        bool      m_freeze;                  /// true时表示只读状态，初始化时，如果没有二级索引则置为false
+        second_index_t m_last_si;            ///< set过程中记录最后一个二级索引
+        vector<second_index_t> second_index; ///< 二级索引
 
         disk_indexer();
         disk_indexer(const disk_indexer&);
     public:
+        enum
+        {
+            OK = 0,
+            READ_ONLY = -1, ///< 调用过set_finish()后，变为只读状态;
+            NOT_READY = -2, ///< 正在写入过程中，不提供查询服务
+        };
         disk_indexer(const char* dir, const char* iname);
         ~disk_indexer();
+        /*
+         * set和get接口是相当危险的，这里的默认时，当连续的set之后，调用set_finish()方法之后才可以调用get方法
+         * 一旦get之后，set接口就失效了。
+         * 这相当于在磁盘上把索引生成之后，才可以使用这个索引。
+         */
         int32_t get_posting_list(const char* strTerm, char* buff, const uint32_t length);
         /*这个接口只能用于连续写入*/
         int32_t set_posting_list(const uint32_t id, const ikey_t& ikey, const char* buff, const uint32_t length);
+        void    set_finish();
 };
 #endif
