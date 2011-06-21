77d3270d88176f8916b2c3551d31211a98a634a9
scenbuffalo@gmail.com <scenbuffalo@gmail.com@b3441f12-2e28-7e8c-f244-a24f140ef41e>
2011-06-21 16:06:21 +0000

    index_group code done

--- trunk/utils/indexer/index_group/index_group.cpp
+++ trunk/utils/indexer/index_group/index_group.cpp
@@ -1,8 +1,120 @@
 #include "index_group.h"
+#include "MyException.h"
+#include "mylog.h"
+#include <stdio.h>
 
-index_group::index_group()
+const char* const STR_INDEX_NAME = "index";
+const char* const STR_INDEX_CUR_NO_FILE = "cur";
+const char* const STR_DAY_INDEX_DIR = "./data/day/";
+const char* const STR_HIS_INDEX_DIR = "./data/his/";
+const char* const STR_FMT_DAY_INDEX_PATH = "./data/day/%u/";
+const char* const STR_FMT_HIS_INDEX_PATH = "./data/his/%u/";
+
+index_group :: index_group()
+{
+    index_info_t index_info;
+
+    // mem init
+    index_info.empty  = true;
+    index_info.pindex = &m_mem[m_mem_cur];
+    m_index_list[0] = index_info;
+    index_info.empty  = true;
+    index_info.pindex = &m_mem[1 - m_mem_cur];
+    m_index_list[1] = index_info;
+
+    // day init
+    // read cur file
+    uint32_t day_cur = get_cur_no(STR_DAY_INDEX_DIR, STR_INDEX_CUR_NO_FILE);
+    index_info.empty  = true;
+    index_info.pindex = &m_day[day_cur];
+    m_index_list[2] = index_info;
+
+    // mon init
+    // read cur file
+    uint32_t his_cur = get_cur_no(STR_HIS_INDEX_DIR, STR_INDEX_CUR_NO_FILE);
+    index_info.empty  = true;
+    index_info.pindex = &m_his[his_cur];
+    m_index_list[3] = index_info;
+
+    pthread_mutex_init(&m_mutex);
+}
+
+index_group :: ~index_group()
 {
 }
-index_group::~index_group()
+
+mem_indexer* index_group :: swap_mem_indexer(mem_indexer* pmem_indexer)
 {
+    MyThrowAssert(m_index_list[0].pindex == pmem_indexer);
+    // 阻塞于等待 m_index_list[1].empty == true;
+    while(m_index_list[1].empty == true)
+    {
+        pthread_cond_wait(m_mem_dump_cond);
+    }
+    // -1- 调换位置
+    pthread_mutex_lock(&m_mutex);
+    m_index_list[0].empty = false;
+    index_info_t tmp_index_info = m_index_list[0];
+    m_index_list[0] = m_index_list[1];
+    m_index_list[1] = tmp_index_info;
+    pthread_mutex_unlock(&m_mutex);
+    // -2- 通知merge线程可以把mem持久化了
+    pthread_cond_signal(m_mem_dump_cond);
+    // -3- 返回一个新的mem
+    return m_index_list[0].pindex;
+}
+
+void index_group :: update_day_indexer(disk_indexer* pdisk_indexer)
+{
+    pthread_mutex_lock(&m_mutex);
+    MyThrowAssert(pdisk_indexer == &m_day[0] || pdisk_indexer == &m_day[1]);
+    MyThrowAssert(pdisk_indexer != &m_index_list[2].pindex);
+    m_index_list[1].reset();
+    m_index_list[1].empty = true;
+    m_index_list[2].reset();
+    m_index_list[2].pindex = pdisk_indexer;
+    pthread_mutex_unlock(&m_mutex);
+}
+
+void index_group :: update_his_indexer(disk_indexer* pdisk_indexer)
+{
+    pthread_mutex_lock(&m_mutex);
+    MyThrowAssert(pdisk_indexer == &m_his[0] || pdisk_indexer == &m_his[1]);
+    MyThrowAssert(pdisk_indexer != &m_index_list[3].pindex);
+    m_index_list[2].reset();
+    m_index_list[2].empty = true;
+    m_index_list[3].reset();
+    m_index_list[3].pindex = pdisk_indexer;
+    pthread_mutex_unlock(&m_mutex);
+}
+
+mem_indexer*  index_group :: get_cur_mem_indexer()
+{
+    return m_index_list[0].pindex;
+}
+disk_indexer* index_group :: get_cur_day_indexer()
+{
+    return m_index_list[2].pindex;
+}
+disk_indexer* index_group :: get_cur_his_indexer()
+{
+    return m_index_list[3].pindex;
+}
+
+int32_t index_group :: get_posting_list(const char* strTerm, void* buff, const uint32_t length)
+{
+    int32_t offset = 0;
+    int32_t lstnum = 0;
+    pthread_mutex_lock(&m_mutex);
+    for (uint32_t i=0; i<m_index_list.size(); i++)
+    {
+        if (m_index_list[i].empty)
+        {
+            continue;
+        }
+        lstnum += m_index_list[i].pindex->get_posting_list(strTerm, &((char*)buff)[offset], length-offset);
+        offset = lstnum * m_cell_size;
+    }
+    pthread_mutex_unlock(&m_mutex);
+    return lstnum;
 }
--- trunk/utils/indexer/index_group/index_group.h
+++ trunk/utils/indexer/index_group/index_group.h
@@ -1,5 +1,8 @@
 #ifndef _INDEX_GROUP_H_
 #define _INDEX_GROUP_H_
+#include "mem_indexer.h"
+#include "disk_indexer.h"
+#include <pthread.h>
 
 class index_group
 {
@@ -9,9 +12,43 @@ class index_group
             DAY = 1,
             MON = 2
         };
+        mem_indexer  m_mem[2];
+        disk_indexer m_day[2];
+        disk_indexer m_his[2];
+        struct index_info_t
+        {
+            base_index* pindex;
+            bool        empty;
+        };
+
+        // TODO 读写锁效率更好一点
+        pthread_mutex_t m_mutex;
+
+        vector<index_info_t> m_index_list[4];
+
+        uint32_t get_cur_no(const char* dir, const char* file);
     public:
         index_group();
         ~index_group();
+        // 当update_thread写满(or定期?)一个mem时:
+        // -1- 把这个mem放入group中，调换mem的位置(保证各个索引的ID顺序)
+        // -2- 返回另一个空闲的mem(swap到头部了)给update_thread继续写
+        // -3- 启动持久化过程(直接dump或merge)
+        mem_indexer*  swap_mem_indexer(mem_indexer*);
+        // 更新day_indexer时:
+        // -1- 替换掉m_index_list[2]中的disk_indexer
+        // -2- 把m_index_list[1]中的mem_index标记为free状态
+        void          update_day_indexer(disk_indexer*);
+        // 更新his_indexer时:
+        // -1- 替换掉m_index_list[3]中的disk_indexer
+        // -2- 把m_index_list[2]中的disk_index标记为free状态
+        // -3- 检查是否有mem需要直接写入day_indexer
+        void          update_his_indexer(disk_indexer*);
+        mem_indexer*  get_cur_mem_indexer();
+        disk_indexer* get_cur_day_indexer();
+        disk_indexer* get_cur_his_indexer();
+
+        int32_t get_posting_list(const char* strTerm, void* buff, const uint32_t length);
 };
 
 #endif
