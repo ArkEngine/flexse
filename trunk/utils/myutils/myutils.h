2407fbefb4561fe9b89b71366b8ba071249f84e6
scenbuffalo@gmail.com <scenbuffalo@gmail.com@b3441f12-2e28-7e8c-f244-a24f140ef41e>
2011-08-10 12:48:27 +0000

    add equeue and myutils

---
+++ trunk/utils/myutils/myutils.h
@@ -0,0 +1,17 @@
+#ifndef _MYUTILS_H_
+#define _MYUTILS_H_
+#include <stdint.h>
+#include <string>
+#include <vector>
+#include <set>
+using namespace std;
+
+namespace flexse {
+
+    int mylisten(const uint16_t port);
+
+    int setnonblock(int32_t fd);
+
+    void strspliter(char* str, vector<string>& vstr);
+}
+#endif
