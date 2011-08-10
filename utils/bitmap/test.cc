f9a951061e3301126d1a1198ab4e3b25278c1316
scenbuffalo@gmail.com <scenbuffalo@gmail.com@b3441f12-2e28-7e8c-f244-a24f140ef41e>
2011-08-10 01:25:14 +0000

    company update

--- utils/bitmap/test.cc
+++ utils/bitmap/test.cc
@@ -11,7 +11,16 @@ int main(int argc, char** argv)
         return 1;
     }
     const uint32_t SIZE = atoi(argv[1]);
-    bitmap mybitmap("./", "mem.test", SIZE);
+    bitmap* pmybitmap = NULL;
+
+    pmybitmap = new bitmap("./", "mem.test", SIZE);
+    bitmap& mybitmap = *pmybitmap;
+
+    for (uint32_t i=0; i<SIZE; i+=2)
+    {
+        _SET_BITMAP_0_(mybitmap, i);
+        assert(0 == _GET_BITMAP_(mybitmap, i));
+    }
 
     for (uint32_t i=0; i<SIZE; i++)
     {
@@ -52,10 +61,76 @@ int main(int argc, char** argv)
         assert(0 == _GET_BITMAP_(mybitmap, i));
     }
 
+    // delete前的设置0101010101...
     for (uint32_t i=0; i<SIZE; i++)
     {
+        _SET_BITMAP_0_(mybitmap, i);
         assert(0 == _GET_BITMAP_(mybitmap, i));
     }
-    remove("mem.test");
+
+    for (uint32_t i=1; i<SIZE; i+=2)
+    {
+        _SET_BITMAP_1_(mybitmap, i);
+        assert(1 == _GET_BITMAP_(mybitmap, i));
+    }
+
+
+    delete pmybitmap;
+
+    pmybitmap = new bitmap("./", "mem.test", SIZE);
+    bitmap& mmbitmap = *pmybitmap;
+
+    for (uint32_t i=0; i<SIZE; i+=2)
+    {
+        assert(0 == _GET_BITMAP_(mmbitmap, i));
+    }
+
+    for (uint32_t i=1; i<SIZE; i+=2)
+    {
+        assert(1 == _GET_BITMAP_(mmbitmap, i));
+    }
+
+    for (uint32_t i=0; i<SIZE; i+=2)
+    {
+        _SET_BITMAP_1_(mmbitmap, i);
+        assert(1 == _GET_BITMAP_(mmbitmap, i));
+    }
+
+    for (uint32_t i=0; i<SIZE; i+=2)
+    {
+        assert(1 == _GET_BITMAP_(mmbitmap, i));
+    }
+
+    for (uint32_t i=0; i<SIZE; i+=2)
+    {
+        _SET_BITMAP_0_(mmbitmap, i);
+        assert(0 == _GET_BITMAP_(mmbitmap, i));
+    }
+
+    for (uint32_t i=1; i<SIZE; i+=2)
+    {
+        _SET_BITMAP_1_(mmbitmap, i);
+        assert(1 == _GET_BITMAP_(mmbitmap, i));
+    }
+
+    for (uint32_t i=1; i<SIZE; i+=2)
+    {
+        assert(1 == _GET_BITMAP_(mmbitmap, i));
+    }
+
+    for (uint32_t i=1; i<SIZE; i+=2)
+    {
+        _SET_BITMAP_0_(mmbitmap, i);
+        assert(0 == _GET_BITMAP_(mmbitmap, i));
+    }
+
+    for (uint32_t i=0; i<SIZE; i++)
+    {
+        _SET_BITMAP_0_(mmbitmap, i);
+        assert(0 == _GET_BITMAP_(mmbitmap, i));
+    }
+
+    delete pmybitmap;
+
     return 0;
 }
