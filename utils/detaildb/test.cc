15b017a79d8a3d83815cf4108f6c95553663e715
scenbuffalo@gmail.com <scenbuffalo@gmail.com@b3441f12-2e28-7e8c-f244-a24f140ef41e>
2011-08-19 08:00:34 +0000

    detaildb test good

--- utils/detaildb/test.cc
+++ utils/detaildb/test.cc
@@ -1,19 +1,25 @@
 #include <stdio.h>
+#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include "detaildb.h"
 
-int main()
+int main(int argc, char** argv)
 {
+    if (argc != 2)
+    {
+        printf("./test SIZE\n");
+        exit(1);
+    }
+    const uint32_t loopcount = atoi(argv[1]);
     detaildb dtdb("./data/", "test");
     const uint32_t SIZE = 100;
     char* str = (char*)malloc(SIZE);
     memset(str, 'A', SIZE);
-    for (uint32_t i=0; i<100; i++)
+    for (uint32_t i=0; i<loopcount; i++)
     {
-        dtdb.set(i, str, SIZE);
+        assert (0 == dtdb.set(i, str, SIZE));
     }
-    printf("hello world.");
     return 0;
 }
