1a91477b4fdf7e77c2a53f4061affdf8b13f9b53
scenbuffalo@gmail.com <scenbuffalo@gmail.com@b3441f12-2e28-7e8c-f244-a24f140ef41e>
2011-09-04 07:21:19 +0000

    plugin doing

--- plugin/flexse/flexse_plugin.cpp
+++ plugin/flexse/flexse_plugin.cpp
@@ -2,10 +2,24 @@
 #include "MyException.h"
 #include <iostream>
 #include <fstream>
+#include <string.h>
 using namespace std;
 
-const char* const flexse_plugin:: CONFIGCATEGORY_FLEXINDEX  = "FLEXINDEX";
-const char* const flexse_plugin:: CONFIGCATEGORY_STRUCTMASK = "STRUCTMASK";
+const char* const flexse_plugin:: CONFIGCATEGORY_FLEXINDEX      = "FLEXINDEX";
+const char* const flexse_plugin:: FLEXINDEX_KEY_OP              = "op";
+const char* const flexse_plugin:: FLEXINDEX_KEY_TYPE            = "type";
+const char* const flexse_plugin:: FLEXINDEX_KEY_TOKEN           = "token";
+const char* const flexse_plugin:: FLEXINDEX_KEY_FIELD           = "field";
+const char* const flexse_plugin:: CONFIGCATEGORY_STRUCTMASK     = "STRUCTMASK";
+const char* const flexse_plugin:: STRUCTMASK_POST               = "posting_list_cell";
+const char* const flexse_plugin:: STRUCTMASK_ATTR               = "document_attribute";
+const char* const flexse_plugin:: FLEXINDEX_VALUE_OP_NLP        = "NLP";
+const char* const flexse_plugin:: FLEXINDEX_VALUE_OP_DOC_ID     = "DOC_ID";
+const char* const flexse_plugin:: FLEXINDEX_VALUE_OP_PREFIX     = "PREFIX";
+const char* const flexse_plugin:: FLEXINDEX_VALUE_TYPE_INT      = "INT";
+const char* const flexse_plugin:: FLEXINDEX_VALUE_TYPE_STR      = "STR";
+const char* const flexse_plugin:: FLEXINDEX_VALUE_TYPE_LIST_INT = "LIST_INT";
+const char* const flexse_plugin:: FLEXINDEX_VALUE_TYPE_LIST_STR = "LIST_STR";
 
 flexse_plugin:: flexse_plugin(const char* config_path, secore* insecore)
 {
@@ -17,32 +31,84 @@ flexse_plugin:: flexse_plugin(const char* config_path, secore* insecore)
     ifstream in(config_path);
     MySuicideAssert (reader.parse(in, root));
 
+    m_post_maskmap = new structmask(root[CONFIGCATEGORY_STRUCTMASK][STRUCTMASK_POST]);
+    m_attr_maskmap = new structmask(root[CONFIGCATEGORY_STRUCTMASK][STRUCTMASK_ATTR]);
+
     Json::Value flexindex = root[CONFIGCATEGORY_FLEXINDEX];
     MySuicideAssert (!flexindex.isNull());
     Json::Value::const_iterator iter;          //迭代器
     iter = flexindex.begin();
 
-    printf ("size: %d\n", flexindex.size());          // 输出 key1,key2
     for (uint32_t i=0; i<flexindex.size(); i++)
     {
+        key_op_t mkey_op;
+        memset(&mkey_op, 0, sizeof(mkey_op));
+
         Json::Value field = flexindex[i];
         Json::Value::Members member=(*iter).getMemberNames();
 
         string strkey(*(member.begin()));
-        printf ("k: %s\n", strkey.c_str());          // 输出 key1,key2
+        MyThrowAssert(m_key_op_map.end() == m_key_op_map.find(strkey));
         iter++;
-    }
 
-//    for(iter = flexindex.begin();iter != flexindex.end();iter++ )
-//    {
-//        Json::Value::Members member=(*iter).getMemberNames();
-//        printf ("k: %s\n", string(*(member.begin())).c_str());          // 输出 key1,key2
-//        //        (*iter)[*(member.begin())];     //输出 value1,value2
-//    }
+        // 解析处置的方法
+        Json::Value value = field[strkey.c_str()];
+        MyThrowAssert(!value.isNull() && value[FLEXINDEX_KEY_OP].isString());
+        if (0 == strcmp(value[FLEXINDEX_KEY_OP].asCString(), FLEXINDEX_VALUE_OP_DOC_ID))
+        {
+            // 该字段作为ID处理
+            mkey_op.op   = OP_DOC_ID;
+            mkey_op.type = T_INT;
+            MyThrowAssert(value[FLEXINDEX_KEY_FIELD].isString());
+            MyThrowAssert(0 == m_post_maskmap->get_mask_item(value[FLEXINDEX_KEY_FIELD].asCString(), &mkey_op.key_mask));
+        }
+        else if (0 == strcmp(value[FLEXINDEX_KEY_OP].asCString(), FLEXINDEX_VALUE_OP_NLP))
+        {
+            // 该字段要做分词处理
+            mkey_op.op   = OP_NLP;
+            mkey_op.type = T_STR;
+        }
+        else if (0 == strcmp(value[FLEXINDEX_KEY_OP].asCString(), FLEXINDEX_VALUE_OP_PREFIX))
+        {
+            // 该字段要做前缀
+            mkey_op.op   = OP_PREFIX;
+            MyThrowAssert(value[FLEXINDEX_KEY_TYPE].isString());
+            if (0 == strcmp(value[FLEXINDEX_KEY_TYPE].asCString(), FLEXINDEX_VALUE_TYPE_INT))
+            {
+                mkey_op.type = T_INT;
+            }
+            else if (0 == strcmp(value[FLEXINDEX_KEY_TYPE].asCString(), FLEXINDEX_VALUE_TYPE_STR))
+            {
+                mkey_op.type = T_STR;
+            }
+            else if (0 == strcmp(value[FLEXINDEX_KEY_TYPE].asCString(), FLEXINDEX_VALUE_TYPE_LIST_INT))
+            {
+                mkey_op.type = T_LIST_INT;
+            }
+            else if (0 == strcmp(value[FLEXINDEX_KEY_TYPE].asCString(), FLEXINDEX_VALUE_TYPE_LIST_STR))
+            {
+                mkey_op.type = T_LIST_STR;
+            }
+            else
+            {
+                FATAL("unknown type type[%s]", value[FLEXINDEX_KEY_TYPE].asCString());
+                MySuicideAssert(0);
+            }
+        }
+        else
+        {
+            FATAL("unknown op type[%s]", value[FLEXINDEX_KEY_OP].asCString());
+            MySuicideAssert(0);
+        }
+
+        m_key_op_map[strkey] = mkey_op;
+    }
 }
 
 flexse_plugin:: ~flexse_plugin()
 {
+    delete m_post_maskmap;
+    delete m_attr_maskmap;
 }
 
 int flexse_plugin:: add(const char* jsonstr, uint32_t& doc_id, vector<term_info_t> & termlist)
@@ -55,15 +121,29 @@ int flexse_plugin:: add(const char* jsonstr, uint32_t& doc_id, vector<term_info_
         return -1;
     }
 
-    if (root["DOC_ID"].isNull() || ! root["DOC_ID"].isInt() || 0 == root["DOC_ID"].asInt())
+    if (root[FLEXINDEX_VALUE_OP_DOC_ID].isNull()
+            || ! root[FLEXINDEX_VALUE_OP_DOC_ID].isInt()
+            || 0 == root[FLEXINDEX_VALUE_OP_DOC_ID].asInt())
     {
         ALARM("jsonstr NOT contain 'DOC_ID' or Type error.");
         return -1;
     }
     else
     {
-        doc_id = root["DOC_ID"].asInt();
+        doc_id = root[FLEXINDEX_VALUE_OP_DOC_ID].asInt();
     }
+
+    // 从 m_key_op_map 迭代处理
+    map<string, key_op_t>::iterator it;
+    for (it=m_key_op_map.begin(); it!=m_key_op_map.end(); it++)
+    {
+        if (root[it->first.c_str()].isNull())
+        {
+            ALARM("jsonstr NOT contain '%s'.", it->first.c_str());
+            return -1;
+        }
+    }
+    // 从 m_attr_maskmap 迭代处理
 
     if (root["CONTENT"].isNull() || !root["CONTENT"].isString())
     {
@@ -88,14 +168,14 @@ int flexse_plugin:: mod(const char* jsonstr, uint32_t& doc_id, vector<term_info_
         return -1;
     }
 
-    if (root["DOC_ID"].isNull())
+    if (root[FLEXINDEX_VALUE_OP_DOC_ID].isNull())
     {
         ALARM("jsonstr NOT contain 'DOC_ID'.");
         return -1;
     }
     else
     {
-        doc_id = root["DOC_ID"].asInt();
+        doc_id = root[FLEXINDEX_VALUE_OP_DOC_ID].asInt();
     }
 
     if (root["CONTENT"].isNull() || !root["CONTENT"].isString())
