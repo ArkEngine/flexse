0031cfa94dcbe8c0aec0c52c8c6e6302d64d7bdf
scenbuffalo@gmail.com <scenbuffalo@gmail.com@b3441f12-2e28-7e8c-f244-a24f140ef41e>
2011-09-14 07:47:37 +0000

    space bugfix

--- trunk/flexse/test/update.py
+++ trunk/flexse/test/update.py
@@ -1,21 +1,35 @@
 import socket, struct, sys, json
 
 if len(sys.argv) != 4:
-    print "python test.py ip port loopcount"
+    print "python flexseclt.py begin end step"
     sys.exit(1)
+if int(sys.argv[1]) > int(sys.argv[2]) :
+    print "begin > end"
+    sys.exit(1)
+
+begin = int(sys.argv[1])
+end   = int(sys.argv[2]) + 1
+step  = int(sys.argv[3])
 
 FMT_XHEAD = "I16sIII"
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((sys.argv[1], int(sys.argv[2])));
-for x in range(1, 1+int(sys.argv[3])):
-    dd = {}
-    dd['OPERATION'] = "INSERT"
-    dd['DOC_ID'] = x
-    dd['CONTENT'] = "this is test message. no[%u] jingle[%u] buffalo[%u]" % (x, x, x, )
-    #dd['CONTENT'] = "buffalo[%u]" % (x, )
-    jsonstr = json.dumps(dd)
-    sbuf = struct.pack(FMT_XHEAD, x, "pyclient", 0, 0, len(jsonstr))
-    sbuf += jsonstr
-    sock.send(sbuf)
-    rbuf = sock.recv(32)
-#    print struct.unpack(FMT_XHEAD, rbuf)
+sock.connect(("127.0.0.1", 1984));
+for x in range(begin, end):
+    for k in range(0, 10):
+        dd = {}
+        dd['OPERATION'] = "INSERT"
+        dd['id'] = x + (step * 10 + k) * ( end-1 )
+        dd['uid'] = x
+        dd['title'] = "awesome#%u# search#%u# framwork#%u# " % (x, x, x)
+        dd['type'] = x
+        dd['content'] = "a[%u] b[%u] c[%u] d[%u] e[%u] f[%u] g[%u] h[%u] i[%u] j[%u]"\
+                " k[%u] l[%u] m[%u] n[%u] o[%u] p[%u] q[%u] r[%u] s[%u] t[%u] " % \
+                (x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,)
+        dd['tags'] = ["nasty*"+str(x), "sexy*"+str(x), "pretty*"+str(x)]
+        jsonstr = json.dumps(dd)
+#        print jsonstr
+        sbuf = struct.pack(FMT_XHEAD, 123, "pyclient", 0, 0, len(jsonstr))
+        sbuf += jsonstr
+        sock.send(sbuf)
+        rbuf = sock.recv(32)
+#        print struct.unpack(FMT_XHEAD, rbuf)
