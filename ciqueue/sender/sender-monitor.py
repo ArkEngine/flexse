c34401a2038ec4fe60ebd0e0fea77350caa35618
scenbuffalo@gmail.com <scenbuffalo@gmail.com@b3441f12-2e28-7e8c-f244-a24f140ef41e>
2011-10-11 03:12:56 +0000

    bugfix the sender-monitor.py

--- ciqueue/sender/sender-monitor.py
+++ ciqueue/sender/sender-monitor.py
@@ -3,11 +3,32 @@
 # 仅对配置中enable为1的消息通道进行监控
 # 当一个消息通道由正常变为异常时，会发送一个警报
 # 当一个消息通道由异常变为正常时，会发送一个喜报
-# 可能存在的问题，当消息队列超过检查间隔期间没有收到任何消息时，会误报
-import json, sys, time
+# 可能存在的问题，不能检查到更新进度落后太多的问题，当程序性能很好时，这个问题是不会暴露的
+# 如果需要考虑，则比较消息队列的最新进度和消息通道的当前进度即可。
+import json, sys, time, os
 
-def get_offset(offset_file):
-    # 读取offset文件夹下的进度文件
+def get_queue_offset(queue_file_dir, queue_file_name):
+    # 读取这个消息队列的最新进度
+    # 以最大的文件号和最大文件号的文件长度为标识
+    # 如下
+    # {'last_file_len': 3997440L, 'last_file_no': 0}
+    filelist = os.listdir(queue_file_dir)
+    max_file_no = -1
+    prefix = queue_file_name+"."
+    for qfile in filelist:
+        if 0 == qfile.find(prefix):
+            # 找到了一个文件
+            cur_file_no = int(qfile.replace(prefix, ''))
+            max_file_no = max(cur_file_no, max_file_no)
+    assert max_file_no >= 0
+    last_file_len = os.path.getsize(queue_file_dir + "/" + prefix + str(max_file_no))
+    queue_last_offset = {}
+    queue_last_offset['last_file_no']  = max_file_no
+    queue_last_offset['last_file_len'] = last_file_len
+    return queue_last_offset
+
+def get_channel_offset(offset_file):
+    # 读取offset文件夹下的每个消息通道进度文件
     # 以键值对的方式放入一个字典中并返回
     # 字典内容如下
     # {'offset': 833523976, 'block_id': 1615361, 'file_no': 4}
@@ -35,6 +56,13 @@ if check_interval_seconds < 10 :
     sys.exit(1)
 config_file = sender_runing_dir+"/conf/sender.config.json"
 cur_queue_status={}
+try:
+    json_config = json.load(file(config_file))
+except IOError:
+    print "file[%s] NOT exist." % (config_file)
+    sys.exit(1)
+queue_file_dir = sender_runing_dir + "/data/" + json_config["qpath"]
+queue_last_offset = get_queue_offset(queue_file_dir, json_config["qfile"])
 while True:
     # 每次重新读取配置文件
     try:
@@ -42,13 +70,19 @@ while True:
     except IOError:
         print "file[%s] NOT exist." % (config_file)
         sys.exit(1)
+    # 获取消息队列的最大进度
+    cur_last_offset = get_queue_offset(queue_file_dir, json_config["qfile"])
+    queue_changed = (queue_last_offset != cur_last_offset)
+    queue_last_offset = cur_last_offset
+    # 调试日志
+    print "queue_changed: %u" % (queue_changed, )
 
     for x in json_config["followers"]:
-        # 对每个消息通道进行检查
-        if x["enable"]:
+        # 仅当消息队列的最新进度发生变化时(有新的消息进入消息队列了)，对每个消息通道进行检查
+        if x["enable"] and queue_changed:
             # 如果这个通道是开启的，则开始检查进度文件
             offset_file = sender_runing_dir+"/offset/"+x['name']+".offset"
-            new_qoffset = get_offset(offset_file)
+            new_qoffset = get_channel_offset(offset_file)
             if cur_queue_status.has_key(x['name']):
                 old_qoffset = cur_queue_status[x['name']]['qoffset']
                 status_ok = cur_queue_status[x['name']]['status_ok']
@@ -57,7 +91,7 @@ while True:
                     print "queue[%s] update STOP. remote[%s:%u]" % (x['name'], x['host'], x['port'],)
                     cur_queue_status[x['name']]['status_ok'] = False
                 elif new_qoffset == old_qoffset and status_ok == False:
-                    # 这个不需要报警了
+                    # 这个不需要报警了, 为调试日志
                     print "I know this guy[%s - %s:%u] in trouble now."  % (x['name'], x['host'], x['port'],)
                 elif new_qoffset != old_qoffset and status_ok == False:
                     # 异常状态 变成 正常状态，需要发送喜报
@@ -65,7 +99,7 @@ while True:
                     cur_queue_status[x['name']]['qoffset'] = new_qoffset
                     cur_queue_status[x['name']]['status_ok'] = True
                 elif new_qoffset != old_qoffset and status_ok == True:
-                    # 这个也不需要报警了
+                    # 这个不需要报警了, 为调试日志
                     print "I know this guy[%s - %s:%u] is OK."  % (x['name'], x['host'], x['port'],)
                     cur_queue_status[x['name']]['qoffset'] = new_qoffset
             else:
