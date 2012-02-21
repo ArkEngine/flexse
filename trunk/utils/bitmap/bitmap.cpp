51f3137c0178d031da5a9e979bbf2718a4196783
scenbuffalo@gmail.com <scenbuffalo@gmail.com@b3441f12-2e28-7e8c-f244-a24f140ef41e>
2012-02-21 14:49:05 +0000

    bitmap bugfix

--- trunk/utils/bitmap/bitmap.cpp
+++ trunk/utils/bitmap/bitmap.cpp
@@ -10,14 +10,25 @@
 bitmap :: bitmap (const char* dir, const char* file, const uint32_t filesize)
 {
     m_filesize = filesize;
-    MySuicideAssert((0 == (m_filesize % sizeof(uint32_t))) && m_filesize > 0);
+    MySuicideAssert(m_filesize > 0);
     char filename[MAX_FILENAME_LENGTH];
     snprintf(filename, sizeof(filename), "%s/%s", dir, file);
     mode_t amode = (0 == access(filename, F_OK)) ? O_RDWR : O_RDWR|O_CREAT ;
     m_fd = open(filename, amode, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
     MySuicideAssert(m_fd != -1);
     MySuicideAssert (-1 != lseek(m_fd, filesize-1, SEEK_SET));
-    MySuicideAssert ( 1 == write(m_fd, "", 1));
+    char tmpchar;
+    if (1 != read(m_fd, &tmpchar, 1))
+    {
+        MySuicideAssert ( 1 == write(m_fd, "", 1));
+        printf("char[null]\n");
+    }
+    else
+    {
+        MySuicideAssert (-1 != lseek(m_fd, filesize-1, SEEK_SET));
+        MySuicideAssert ( 1 == write(m_fd, &tmpchar, 1));
+        printf("char[%c]\n", tmpchar);
+    }
     MySuicideAssert (MAP_FAILED != (puint = (uint32_t*)mmap(0, m_filesize,
                     PROT_READ | PROT_WRITE, MAP_SHARED, m_fd, 0)));
 }
